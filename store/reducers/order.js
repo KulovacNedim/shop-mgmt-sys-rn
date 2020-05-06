1409e4d40c022653e8503ccb3058d5c41d203acb
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-05-07 01:06:16 +0200

    OrderItem updated to contain trash icon and removing order logic implemented. On click to OrderItem OrderDetailModal is opened and order details are rendered. redux logic to support those operations added.

--- store/reducers/order.js
+++ store/reducers/order.js
@@ -1,17 +1,28 @@
-import * as OrderActions from '../actions/order';
+import * as orderActions from '../actions/order';
 import Order from '../../models/order';
 
 const initialState = {
-    orders: []
+    orders: [],
+    selectedOrder: null
 };
 
 export default (state = initialState, action) => {
     switch (action.type) {
-        case OrderActions.ADD_ORDER:
-            const newOrder = new Order(new Date().toString, action.orderData.items, action.orderData.amount, new Date());
+        case orderActions.ADD_ORDER:
+            const newOrder = new Order(new Date(), action.orderData.items, action.orderData.amount, new Date());
             return {
                 ...state,
                 orders: state.orders.concat(newOrder)
+            };
+        case orderActions.SELECT_ORDER:
+            return {
+                ...state,
+                selectedOrder: action.order
+            };
+        case orderActions.REMOVE_ORDER:
+            return {
+                ...state,
+                orders: state.orders.filter(order => order.id !== action.order.id)
             };
         default:
             return state;
